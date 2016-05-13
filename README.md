81fb154b94876ab7a43f837183191f77cf37f1b9
Evert Timberg <evert.timberg@gmail.com>
2016-05-12 22:11:54 -0400

    Initial commit for Chartjs annotation plugin

---
+++ README.md
@@ -0,0 +1,49 @@
+# Chart.Annotation.js
+
+An annotation plugin for Chart.js >= 2.1.3
+
+
+## Configuration
+
+To configure the annotations plugin, you can simply add new config options to your chart config.
+
+```javascript
+{
+	annotation: {
+		annotations: [{
+			type: 'line',
+			mode: 'horizontal',
+			scaleID: 'y-axis-0',
+			value: '25',
+			borderColor: 'red',
+			borderWidth: 2
+		}]
+	}
+}
+```
+
+## To-do Items
+The following features still need to be done:
+
+
+## Installation
+
+To download a zip, go to the Chart.Annotation.js on Github
+
+To install via npm / bower:
+
+```bash
+npm install Chart.Annotation.js --save
+```
+
+## Documentation
+
+You can find documentation for Chart.js at [www.chartjs.org/docs](http://www.chartjs.org/docs).
+
+## Contributing
+
+Before submitting an issue or a pull request to the project, please take a moment to look over the [contributing guidelines](https://github.com/chartjs/Chart.Annotation.js/blob/master/CONTRIBUTING.md) first.
+
+## License
+
+Chart.Annotation.js is available under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  util = require('gulp-util'),
+  jshint = require('gulp-jshint'),
+  replace = require('gulp-replace'),
+  insert = require('gulp-insert'),
+  inquirer = require('inquirer'),
+  semver = require('semver'),
+  exec = require('child_process').exec,
+  fs = require('fs'),
+  package = require('./package.json'),
+  browserify = require('browserify'),
+  streamify = require('gulp-streamify'),
+  source = require('vinyl-source-stream'),
+  merge = require('merge-stream');
+
+var srcDir = './src/';
+var outDir = './';
+
+var header = "/*!\n\
+ * Chart.Annotation.js\n\
+ * http://chartjs.org/\n\
+ * Version: {{ version }}\n\
+ *\n\
+ * Copyright 2016 Evert Timberg\n\
+ * Released under the MIT license\n\
+ * https://github.com/chartjs/Chart.Annotation.js/blob/master/LICENSE.md\n\
+ */\n";
+
+gulp.task('build', buildTask);
+gulp.task('bump', bumpTask);
+gulp.task('jshint', jshintTask);
+
+function buildTask() {
+  var nonBundled = browserify('./src/chart.annotation.js')
+    .ignore('chart.js')
+    .ignore('hammerjs')
+    .bundle()
+    .pipe(source('Chart.Annotation.js'))
+    .pipe(insert.prepend(header))
+    .pipe(streamify(replace('{{ version }}', package.version)))
+    .pipe(gulp.dest(outDir))
+    .pipe(streamify(uglify({
+      preserveComments: 'some'
+    })))
+    .pipe(streamify(concat('Chart.Annotation.min.js')))
+    .pipe(gulp.dest(outDir));
+
+  return nonBundled;
+
+}
+
+/*
+ *  Usage : gulp bump
+ *  Prompts: Version increment to bump
+ *  Output: - New version number written into package.json & bower.json
+ */
+function bumpTask(complete) {
+  util.log('Current version:', util.colors.cyan(package.version));
+  var choices = ['major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'prerelease'].map(function(versionType) {
+    return versionType + ' (v' + semver.inc(package.version, versionType) + ')';
+  });
+  inquirer.prompt({
+    type: 'list',
+    name: 'version',
+    message: 'What version update would you like?',
+    choices: choices
+  }, function(res) {
+    var increment = res.version.split(' ')[0],
+      newVersion = semver.inc(package.version, increment);
+
+    // Set the new versions into the bower/package object
+    package.version = newVersion;
+    bower.version = newVersion;
+
+    // Write these to their own files, then build the output
+    fs.writeFileSync('package.json', JSON.stringify(package, null, 2));
+    fs.writeFileSync('bower.json', JSON.stringify(bower, null, 2));
+
+    complete();
+  });
+}
+
+function jshintTask() {
+  return gulp.src(srcDir + '**/*.js')
+    .pipe(jshint('config.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+}
