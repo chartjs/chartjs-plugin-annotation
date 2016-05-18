a028b4bd6141eb4d826ac7d62aaf916b44b94d9e
Evert Timberg <evert.timberg@gmail.com>
2016-05-17 21:50:34 -0400

    Built files for 0.1 release

--- Chart.Annotation.js
+++ Chart.Annotation.js
@@ -10,14 +10,83 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 },{}],2:[function(require,module,exports){
+// Box Annotation implementation
+module.exports = function(Chart) {
+	var BoxAnnotation = Chart.Element.extend({
+		draw: function(ctx) {
+			var view = this._view;
+
+			// Canvas setup
+			ctx.lineWidth = view.borderWidth;
+			ctx.strokeStyle = view.borderColor;
+			ctx.fillStyle = view.backgroundColor;
+
+			// Draw
+			var width = view.right - view.left,
+				height = view.bottom - view.top;
+			ctx.fillRect(view.left, view.top, width, height);
+			ctx.strokeRect(view.left, view.top, width, height)
+		}
+	});
+
+	function isValid(num) {
+		return !isNaN(num) && isFinite(num);
+	}
+
+	// Function that updates a box annotation
+	function boxUpdate(obj, options, chartInstance) {
+		var model = obj._model = obj._model || {};
+
+		var xScale = chartInstance.scales[options.xScaleID];
+		var yScale = chartInstance.scales[options.yScaleID];
+		var chartArea = chartInstance.chartArea;
+
+		var left = chartArea.left, 
+			top = chartArea.top, 
+			right = chartArea.right, 
+			bottom = chartArea.bottom;
+
+		var min,max;
+
+		if (xScale) {
+			min = isValid(options.xMin) ? xScale.getPixelForValue(options.xMin) : chartArea.left;
+			max = isValid(options.xMax) ? xScale.getPixelForValue(options.xMax) : chartArea.right;
+			left = Math.min(min, max);
+			right = Math.max(min, max);
+		}
+
+		if (yScale) {
+			min = isValid(options.yMin) ? yScale.getPixelForValue(options.yMin) : chartArea.bottom;
+			max = isValid(options.yMax) ? yScale.getPixelForValue(options.yMax) : chartArea.top;
+			top = Math.min(min, max);
+			bottom = Math.max(min, max);
+		}
+
+		// Ensure model has rect coordinates
+		model.left = left;
+		model.top = top;
+		model.right = right;
+		model.bottom = bottom;
+
+		// Stylistic options
+		model.borderColor = options.borderColor;
+		model.borderWidth = options.borderWidth;
+		model.backgroundColor = options.backgroundColor;
+	}
+
+
+	return {
+		Constructor: BoxAnnotation,
+		update: boxUpdate
+	};
+}
+
+},{}],3:[function(require,module,exports){
 // Get the chart variable
 var Chart = require('chart.js');
 Chart = typeof(Chart) === 'function' ? Chart : window.Chart;
 var helpers = Chart.helpers;
 var isArray = helpers.isArray;
-
-var horizontalKeyword = 'horizontal';
-var verticalKeyword=  'vertica';
 
 // Take the zoom namespace of Chart
 Chart.Annotation = Chart.Annotation || {};
@@ -27,54 +96,19 @@ var defaultOptions = Chart.Annotation.defaults = {
 	annotations: [] // default to no annotations
 };
 
-var LineAnnotation = Chart.Element.extend({
-
-	draw: function(ctx) {
-		var view = this._view;
-
-		// Canvas setup
-		ctx.lineWidth = view.borderWidth;
-		ctx.strokeStyle = view.borderColor;
-
-		// Draw
-		ctx.beginPath();
-		ctx.moveTo(view.x1, view.y1);
-		ctx.lineTo(view.x2, view.y2);
-		ctx.stroke();
-	}
-});
-
-function lineUpdate(obj, options, chartInstance) {
-	var model = obj._model = obj._model || {};
-
-	var scale = chartInstance.scales[options.scaleID];
-	var pixel = scale ? scale.getPixelForValue(options.value) : NaN;
-	var chartArea = chartInstance.chartArea;
-
-	if (!isNaN(pixel)) {
-		if (options.mode == horizontalKeyword) {
-			model.x1 = chartArea.left;
-			model.x2 = chartArea.right;
-			model.y1 = model.y2 = pixel;
-		} else {
-			model.y1 = chartArea.top;
-			model.y2 = chartArea.bottom;
-			model.x1 = model.x2 = pixel;
-		}
-	}
-
-	model.borderColor = options.borderColor;
-	model.borderWidth = options.borderWidth;
-}
+var lineAnnotation = require('./line.js')(Chart);
+var boxAnnotation = require('./box.js')(Chart);
 
 // Map of all types
 var annotationTypes = Chart.Annotation.annotationTypes = {
-	line: LineAnnotation
+	line: lineAnnotation.Constructor,
+	box: boxAnnotation.Constructor
 };
 
 // Map of all update functions
 var updateFunctions = Chart.Annotation.updateFunctions = {
-	line: lineUpdate,
+	line: lineAnnotation.update,
+	box: boxAnnotation.update
 };
 
 // Chartjs Zoom Plugin
@@ -127,6 +161,60 @@ var AnnotationPlugin = Chart.PluginBase.extend({
 	}
 });
 
+module.exports = AnnotationPlugin;
 Chart.pluginService.register(new AnnotationPlugin());
 
-},{"chart.js":1}]},{},[2]);
+},{"./box.js":2,"./line.js":4,"chart.js":1}],4:[function(require,module,exports){
+// Line Annotation implementation
+module.exports = function(Chart) {
+	var horizontalKeyword = 'horizontal';
+	var verticalKeyword = 'vertical';
+
+	var LineAnnotation = Chart.Element.extend({
+
+		draw: function(ctx) {
+			var view = this._view;
+
+			// Canvas setup
+			ctx.lineWidth = view.borderWidth;
+			ctx.strokeStyle = view.borderColor;
+
+			// Draw
+			ctx.beginPath();
+			ctx.moveTo(view.x1, view.y1);
+			ctx.lineTo(view.x2, view.y2);
+			ctx.stroke();
+		}
+	});
+
+	function lineUpdate(obj, options, chartInstance) {
+		var model = obj._model = obj._model || {};
+
+		var scale = chartInstance.scales[options.scaleID];
+		var pixel = scale ? scale.getPixelForValue(options.value) : NaN;
+		var chartArea = chartInstance.chartArea;
+
+		if (!isNaN(pixel)) {
+			if (options.mode == horizontalKeyword) {
+				model.x1 = chartArea.left;
+				model.x2 = chartArea.right;
+				model.y1 = model.y2 = pixel;
+			} else {
+				model.y1 = chartArea.top;
+				model.y2 = chartArea.bottom;
+				model.x1 = model.x2 = pixel;
+			}
+		}
+
+		model.borderColor = options.borderColor;
+		model.borderWidth = options.borderWidth;
+	}
+
+
+	return {
+		Constructor: LineAnnotation,
+		update: lineUpdate
+	};
+};
+
+},{}]},{},[3]);
