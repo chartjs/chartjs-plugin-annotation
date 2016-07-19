dee003416f46ad73886a3db5bc585b26570943a6
Adam Pine <apine@PC67827.corporate.gannettfleming.com>
2016-07-19 16:19:37 -0400

    changes beforeDraw to beforeDatasetsDraw

--- src/chart.annotation.js
+++ src/chart.annotation.js
@@ -64,7 +64,7 @@ var AnnotationPlugin = Chart.PluginBase.extend({
 		}
 	},
 
-	beforeDraw: function(chartInstance, easingDecimal) {
+	beforeDatasetsDraw: function(chartInstance, easingDecimal) {
 		// If we have annotations, draw them
 		var annotationObjects = chartInstance._annotationObjects;
         var annotationOpts = chartInstance.options.annotation;
