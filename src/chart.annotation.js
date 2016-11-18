9781f6232ac7b53046070a21a34d97eb04dbc599
Jonathon Hill <jhill9693@gmail.com>
2016-11-18 17:13:49 -0500

    Rename variables for readability

--- src/chart.annotation.js
+++ src/chart.annotation.js
@@ -4,7 +4,7 @@ Chart = typeof(Chart) === 'function' ? Chart : window.Chart;
 var helpers = Chart.helpers;
 var isArray = helpers.isArray;
 
-// Take the zoom namespace of Chart
+// Configure plugin namespace
 Chart.Annotation = Chart.Annotation || {};
 
 // Default options if none are provided
@@ -13,11 +13,28 @@ Chart.Annotation.defaults = {
 	annotations: [] // default to no annotations
 };
 
+// Default annotation label options
+Chart.Annotation.labelDefaults = {
+	backgroundColor: 'rgba(0,0,0,0.8)',
+	fontFamily: Chart.defaults.global.defaultFontFamily,
+	fontSize: Chart.defaults.global.defaultFontSize,
+	fontStyle: 'bold',
+	fontColor: '#fff',
+	xPadding: 6,
+	yPadding: 6,
+	cornerRadius: 6,
+	position: 'center',
+	xAdjust: 0,
+	yAdjust: 0,
+	enabled: false,
+	content: null
+};
+
 var lineAnnotation = require('./line.js')(Chart);
 var boxAnnotation = require('./box.js')(Chart);
 
 // Map of all types
-Chart.Annotation.annotationTypes = {
+Chart.Annotation.constructors = {
 	line: lineAnnotation.Constructor,
 	box: boxAnnotation.Constructor
 };
@@ -35,17 +52,17 @@ Chart.Annotation.drawTimeOptions = {
 };
 
 function drawAnnotations(drawTime, chartInstance, easingDecimal) {
-	var annotationOpts = chartInstance.options.annotation;
-	if (annotationOpts.drawTime != drawTime) {
+	var annotationConfigs = chartInstance.options.annotation;
+	if (annotationConfigs.drawTime != drawTime) {
 		return;
 	}
 	// If we have annotations, draw them
-	var annotationObjects = chartInstance._annotationObjects;
+	var annotationObjects = chartInstance.annotations;
 	if (isArray(annotationObjects)) {
 		var ctx = chartInstance.chart.ctx;
 
-		annotationObjects.forEach(function(obj) {
-			obj.transition(easingDecimal).draw(ctx);
+		annotationObjects.forEach(function(annotation) {
+			annotation.transition(easingDecimal).draw(ctx);
 		});
 	}
 }
@@ -53,55 +70,42 @@ function drawAnnotations(drawTime, chartInstance, easingDecimal) {
 // Chartjs Zoom Plugin
 var annotationPlugin = {
 	beforeInit: function(chartInstance) {
-		var options = chartInstance.options;
-		options.annotation = helpers.configMerge(Chart.Annotation.defaults, options.annotation);
-		var defaultLabelOptions = {
-			backgroundColor: 'rgba(0,0,0,0.8)',
-			fontFamily: options.defaultFontFamily,
-			fontSize: options.defaultFontSize,
-			fontStyle: "bold",
-			fontColor: "#fff",
-			xPadding: 6,
-			yPadding: 6,
-			cornerRadius: 6,
-			position: "center",
-			xAdjust: 0,
-			yAdjust: 0,
-			enabled: false,
-			content: null
-		};
-		var annotationConfigs = options.annotation.annotations;
+		chartInstance.options.annotation = helpers.configMerge(
+			Chart.Annotation.defaults,
+			chartInstance.options.annotation
+		);
+		var annotationConfigs = chartInstance.options.annotation.annotations;
 		if (isArray(annotationConfigs)) {
-			var annotationObjects = chartInstance._annotationObjects = [];
+			var annotationObjects = chartInstance.annotations = [];
 
 			annotationConfigs.forEach(function(configuration, i) {
-				configuration.label = helpers.configMerge(defaultLabelOptions, configuration.label);
-				var Constructor = Chart.Annotation.annotationTypes[configuration.type];
-				if (Constructor) {
-					annotationObjects.push(new Constructor({
-						_index: i
-					}));
+				configuration.label = helpers.configMerge(
+					Chart.Annotation.labelDefaults,
+					configuration.label
+				);
+				var annotation = Chart.Annotation.constructors[configuration.type];
+				if (annotation) {
+					annotationObjects.push(new annotation({ _index: i }));
 				}
 			});
 		}
 	},
 	afterScaleUpdate: function(chartInstance) {
 		// Once scales are ready, update
-		var annotationObjects = chartInstance._annotationObjects;
-		var annotationOpts = chartInstance.options.annotation;
+		var annotationObjects = chartInstance.annotations;
+		var annotationConfigs = chartInstance.options.annotation;
 
 		if (isArray(annotationObjects)) {
 			annotationObjects.forEach(function(annotationObject) {
-				var opts = annotationOpts.annotations[annotationObject._index];
-				var updateFunction = Chart.Annotation.updateFunctions[opts.type];
+				var config = annotationConfigs.annotations[annotationObject._index];
+				var updateFunction = Chart.Annotation.updateFunctions[config.type];
 
 				if (updateFunction) {
-					updateFunction(annotationObject, opts, chartInstance);
+					updateFunction(annotationObject, config, chartInstance);
 				}
 			});
 		}
 	},
-
 	afterDraw: function(chartInstance, easingDecimal) {
 		drawAnnotations(
 			Chart.Annotation.drawTimeOptions.afterDraw,
