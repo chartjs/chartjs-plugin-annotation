c5c18b261126f7aaa4f074c17ca1e1b2d450c141
Jonathon Hill <jhill9693@gmail.com>
2017-05-20 14:06:50 -0400

    Bump version to 0.5.5

--- chartjs-plugin-annotation.js
+++ chartjs-plugin-annotation.js
@@ -1,7 +1,7 @@
 /*!
  * chartjs-plugin-annotation.js
  * http://chartjs.org/
- * Version: 0.5.4
+ * Version: 0.5.5
  *
  * Copyright 2016 Evert Timberg
  * Released under the MIT license
@@ -25,6 +25,20 @@ module.exports = function(Chart) {
 		});
 	}
 
+	function draw(drawTime) {
+		return function(chartInstance, easingDecimal) {
+			var defaultDrawTime = chartInstance.annotation.options.drawTime;
+
+			helpers.elements(chartInstance)
+				.filter(function(element) {
+					return drawTime === (element.options.drawTime || defaultDrawTime);
+				})
+				.forEach(function(element) {
+					element.transition(easingDecimal).draw();
+				});
+		};
+	}
+
 	return {
 		beforeInit: function(chartInstance) {
 			var chartOptions = chartInstance.options;
@@ -36,12 +50,6 @@ module.exports = function(Chart) {
 				onDestroy: [],
 				firstRun: true,
 				supported: false
-			};
-
-			ns[ns.options.drawTime] = function(easingDecimal) {
-				helpers.elements(chartInstance).forEach(function(element) {
-					element.transition(easingDecimal).draw();
-				});
 			};
 
 			// Add the annotation scale adjuster to each scale's afterDataLimits hook
@@ -103,15 +111,9 @@ module.exports = function(Chart) {
 				element.configure();
 			});
 		},
-		beforeDatasetsDraw: function(chartInstance, easingDecimal) {
-			(chartInstance.annotation.beforeDatasetsDraw || helpers.noop)(easingDecimal);
-		},
-		afterDatasetsDraw: function(chartInstance, easingDecimal) {
-			(chartInstance.annotation.afterDatasetsDraw || helpers.noop)(easingDecimal);
-		},
-		afterDraw: function(chartInstance, easingDecimal) {
-			(chartInstance.annotation.afterDraw || helpers.noop)(easingDecimal);
-		},
+		beforeDatasetsDraw: draw('beforeDatasetsDraw'),
+		afterDatasetsDraw: draw('afterDatasetsDraw'),
+		afterDraw: draw('afterDraw'),
 		afterInit: function(chartInstance) {
 			// Detect and intercept events that happen on an annotation element
 			var watchFor = chartInstance.annotation.options.events;
@@ -274,6 +276,7 @@ module.exports = function(Chart) {
 function noop() {}
 
 function elements(chartInstance) {
+	// Turn the elements object into an array of elements
 	var elements = chartInstance.annotation.elements;
 	return Object.keys(elements).map(function(id) {
 		return elements[id];
@@ -740,7 +743,20 @@ module.exports = function(Chart) {
 		},
 		inRange: function(mouseX, mouseY) {
 			var model = this._model;
-			return (model.line && model.line.intersects(mouseX, mouseY, this.getHeight()));
+			
+			return (
+				// On the line
+				model.line &&
+				model.line.intersects(mouseX, mouseY, this.getHeight())
+			) || (
+				// On the label
+				model.labelEnabled &&
+				model.labelContent &&
+				mouseX >= model.labelX && 
+				mouseX <= model.labelX + model.labelWidth && 
+				mouseY >= model.labelY && 
+				mouseY <= model.labelY + model.labelHeight
+			);
 		},
 		getCenterPoint: function() {
 			return {
