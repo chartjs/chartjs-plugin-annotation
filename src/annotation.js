5e5bdf704715ca811db23e498b3faad554b90dc4
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-01-04 15:27:36 +0200

    Update dependencies and version to 1.0.0-beta (#291)
    * Update dependencies and version to 1.0.0-beta
    
    * Update eslintrc for samples
    
    * Change indentation to spaces
    
    * Update editorconfig

--- src/annotation.js
+++ src/annotation.js
@@ -9,195 +9,195 @@ import PointAnnotation from './types/point';
 const chartStates = new Map();
 
 const annotationTypes = {
-	box: BoxAnnotation,
-	line: LineAnnotation,
-	ellipse: EllipseAnnotation,
-	point: PointAnnotation
+  box: BoxAnnotation,
+  line: LineAnnotation,
+  ellipse: EllipseAnnotation,
+  point: PointAnnotation
 };
 
 export default {
-	id: 'annotation',
+  id: 'annotation',
 
-	beforeInit(chart) {
-		chartStates.set(chart, {
-			elements: [],
-			listeners: {},
-			listened: false,
-			moveListened: false,
-			scales: new Set()
-		});
-	},
+  beforeInit(chart) {
+    chartStates.set(chart, {
+      elements: [],
+      listeners: {},
+      listened: false,
+      moveListened: false,
+      scales: new Set()
+    });
+  },
 
-	afterDataLimits(chart, args, options) {
-		const state = chartStates.get(chart);
-		adjustScaleRange(args.scale, state, options);
-	},
+  afterDataLimits(chart, args, options) {
+    const state = chartStates.get(chart);
+    adjustScaleRange(args.scale, state, options);
+  },
 
-	beforeUpdate(chart, args, options) {
-		if (isObject(options.annotations)) {
-			const array = new Array();
-			Object.keys(options.annotations).forEach(key => {
-				const value = options.annotations[key];
-				if (isObject(value)) {
-					value.id = key;
-					array.push(value);
-				}
-			});
-			options.annotations = array;
-		}
-	},
+  beforeUpdate(chart, args, options) {
+    if (isObject(options.annotations)) {
+      const array = new Array();
+      Object.keys(options.annotations).forEach(key => {
+        const value = options.annotations[key];
+        if (isObject(value)) {
+          value.id = key;
+          array.push(value);
+        }
+      });
+      options.annotations = array;
+    }
+  },
 
-	afterUpdate(chart, args, options) {
-		const state = chartStates.get(chart);
-		updateListeners(chart, state, options);
-		updateElements(chart, state, options, args.mode);
-	},
+  afterUpdate(chart, args, options) {
+    const state = chartStates.get(chart);
+    updateListeners(chart, state, options);
+    updateElements(chart, state, options, args.mode);
+  },
 
-	beforeDatasetsDraw(chart, _args, options) {
-		draw(chart, options, 'beforeDatasetsDraw');
-	},
+  beforeDatasetsDraw(chart, _args, options) {
+    draw(chart, options, 'beforeDatasetsDraw');
+  },
 
-	afterDatasetsDraw(chart, _args, options) {
-		draw(chart, options, 'afterDatasetsDraw');
-	},
+  afterDatasetsDraw(chart, _args, options) {
+    draw(chart, options, 'afterDatasetsDraw');
+  },
 
-	afterDraw(chart, _args, options) {
-		draw(chart, options, 'afterDraw');
-	},
+  afterDraw(chart, _args, options) {
+    draw(chart, options, 'afterDraw');
+  },
 
-	beforeEvent(chart, args, options) {
-		const state = chartStates.get(chart);
-		handleEvent(chart, state, args.event, options);
-	},
+  beforeEvent(chart, args, options) {
+    const state = chartStates.get(chart);
+    handleEvent(chart, state, args.event, options);
+  },
 
-	destroy(chart) {
-		chartStates.delete(chart);
-	},
+  destroy(chart) {
+    chartStates.delete(chart);
+  },
 
-	defaults: {
-		drawTime: 'afterDatasetsDraw',
-		dblClickSpeed: 350, // ms
-		annotations: {},
-		animation: {
-			numbers: {
-				properties: ['x', 'y', 'x2', 'y2', 'width', 'height'],
-				type: 'number'
-			},
-		}
-	},
+  defaults: {
+    drawTime: 'afterDatasetsDraw',
+    dblClickSpeed: 350, // ms
+    annotations: {},
+    animation: {
+      numbers: {
+        properties: ['x', 'y', 'x2', 'y2', 'width', 'height'],
+        type: 'number'
+      },
+    }
+  },
 };
 
 const directUpdater = {
-	update: Object.assign
+  update: Object.assign
 };
 
 function resolveAnimations(chart, animOpts, mode) {
-	if (mode === 'reset' || mode === 'none' || mode === 'resize') {
-		return directUpdater;
-	}
-	return new Animations(chart, animOpts);
+  if (mode === 'reset' || mode === 'none' || mode === 'resize') {
+    return directUpdater;
+  }
+  return new Animations(chart, animOpts);
 }
 
 function updateElements(chart, state, options, mode) {
-	const chartAnims = chart.options.animation;
-	const animOpts = chartAnims && merge({}, [chartAnims, options.animation]);
-	const animations = resolveAnimations(chart, animOpts, mode);
+  const chartAnims = chart.options.animation;
+  const animOpts = chartAnims && merge({}, [chartAnims, options.animation]);
+  const animations = resolveAnimations(chart, animOpts, mode);
 
-	const annotations = options.annotations || [];
-	const elements = resyncElements(state.elements, annotations);
+  const annotations = options.annotations || [];
+  const elements = resyncElements(state.elements, annotations);
 
-	for (let i = 0; i < annotations.length; i++) {
-		const annotation = annotations[i];
-		let el = elements[i];
-		const elType = annotationTypes[annotation.type] || annotationTypes.line;
-		if (!el || !(el instanceof elType)) {
-			el = elements[i] = new elType();
-		}
-		const display = typeof annotation.display === 'function' ? callCallback(annotation.display, [{chart, element: el}]) : valueOrDefault(annotation.display, true);
-		el._display = !!display;
+  for (let i = 0; i < annotations.length; i++) {
+    const annotation = annotations[i];
+    let el = elements[i];
+    const elType = annotationTypes[annotation.type] || annotationTypes.line;
+    if (!el || !(el instanceof elType)) {
+      el = elements[i] = new elType();
+    }
+    const display = typeof annotation.display === 'function' ? callCallback(annotation.display, [{chart, element: el}]) : valueOrDefault(annotation.display, true);
+    el._display = !!display;
 
-		if (el._display) {
-			const properties = el.resolveElementProperties(chart, annotation);
-			properties.options = merge(Object.create(null), [elType.defaults, annotation]);
-			animations.update(el, properties);
-		}
-	}
+    if (el._display) {
+      const properties = el.resolveElementProperties(chart, annotation);
+      properties.options = merge(Object.create(null), [elType.defaults, annotation]);
+      animations.update(el, properties);
+    }
+  }
 }
 
 function resyncElements(elements, annotations) {
-	const count = annotations.length;
-	const start = elements.length;
+  const count = annotations.length;
+  const start = elements.length;
 
-	if (start < count) {
-		const add = count - start;
-		elements.splice(start, 0, ...new Array(add));
-	} else if (start > count) {
-		elements.splice(count, start - count);
-	}
-	return elements;
+  if (start < count) {
+    const add = count - start;
+    elements.splice(start, 0, ...new Array(add));
+  } else if (start > count) {
+    elements.splice(count, start - count);
+  }
+  return elements;
 }
 
 function draw(chart, options, caller) {
-	const {ctx, chartArea} = chart;
-	const elements = chartStates.get(chart).elements.filter(el => el._display);
+  const {ctx, chartArea} = chart;
+  const elements = chartStates.get(chart).elements.filter(el => el._display);
 
-	clipArea(ctx, chartArea);
-	elements.forEach(el => {
-		if ((el.options.drawTime || options.drawTime || caller) === caller) {
-			el.draw(ctx);
-		}
-	});
-	elements.forEach(el => {
-		if ('drawLabel' in el && el.options.label && (el.options.label.drawTime || el.options.drawTime || options.drawTime || caller) === caller) {
-			el.drawLabel(ctx);
-		}
-	});
-	unclipArea(ctx);
+  clipArea(ctx, chartArea);
+  elements.forEach(el => {
+    if ((el.options.drawTime || options.drawTime || caller) === caller) {
+      el.draw(ctx);
+    }
+  });
+  elements.forEach(el => {
+    if ('drawLabel' in el && el.options.label && (el.options.label.drawTime || el.options.drawTime || options.drawTime || caller) === caller) {
+      el.drawLabel(ctx);
+    }
+  });
+  unclipArea(ctx);
 }
 
 function getAnnotationOptions(elements, options) {
-	if (elements && elements.length) {
-		return elements.map(el => el.options);
-	}
-	return options.annotations || [];
+  if (elements && elements.length) {
+    return elements.map(el => el.options);
+  }
+  return options.annotations || [];
 }
 
 function adjustScaleRange(scale, state, options) {
-	const annotations = getAnnotationOptions(state.elements, options);
-	const range = getScaleLimits(scale, annotations);
-	let changed = false;
-	if (isFinite(range.min) &&
+  const annotations = getAnnotationOptions(state.elements, options);
+  const range = getScaleLimits(scale, annotations);
+  let changed = false;
+  if (isFinite(range.min) &&
 		typeof scale.options.min === 'undefined' &&
 		typeof scale.options.suggestedMin === 'undefined') {
-		changed = scale.min !== range.min;
-		scale.min = range.min;
-	}
-	if (isFinite(range.max) &&
+    changed = scale.min !== range.min;
+    scale.min = range.min;
+  }
+  if (isFinite(range.max) &&
 		typeof scale.options.max === 'undefined' &&
 		typeof scale.options.suggestedMax === 'undefined') {
-		changed = scale.max !== range.max;
-		scale.max = range.max;
-	}
-	if (changed && typeof scale.handleTickRangeOptions === 'function') {
-		scale.handleTickRangeOptions();
-	}
+    changed = scale.max !== range.max;
+    scale.max = range.max;
+  }
+  if (changed && typeof scale.handleTickRangeOptions === 'function') {
+    scale.handleTickRangeOptions();
+  }
 }
 
 function getScaleLimits(scale, annotations) {
-	const axis = scale.axis;
-	const scaleID = scale.id;
-	const scaleIDOption = scale.axis + 'ScaleID';
-	const scaleAnnotations = annotations.filter(annotation => annotation[scaleIDOption] === scaleID || annotation.scaleID === scaleID);
-	let min = valueOrDefault(scale.min, Number.NEGATIVE_INFINITY);
-	let max = valueOrDefault(scale.max, Number.POSITIVE_INFINITY);
-	scaleAnnotations.forEach(annotation => {
-		['value', 'endValue', axis + 'Min', axis + 'Max', 'xValue', 'yValue'].forEach(prop => {
-			if (prop in annotation) {
-				const value = annotation[prop];
-				min = Math.min(min, value);
-				max = Math.max(max, value);
-			}
-		});
-	});
-	return {min, max};
+  const axis = scale.axis;
+  const scaleID = scale.id;
+  const scaleIDOption = scale.axis + 'ScaleID';
+  const scaleAnnotations = annotations.filter(annotation => annotation[scaleIDOption] === scaleID || annotation.scaleID === scaleID);
+  let min = valueOrDefault(scale.min, Number.NEGATIVE_INFINITY);
+  let max = valueOrDefault(scale.max, Number.POSITIVE_INFINITY);
+  scaleAnnotations.forEach(annotation => {
+    ['value', 'endValue', axis + 'Min', axis + 'Max', 'xValue', 'yValue'].forEach(prop => {
+      if (prop in annotation) {
+        const value = annotation[prop];
+        min = Math.min(min, value);
+        max = Math.max(max, value);
+      }
+    });
+  });
+  return {min, max};
 }
--- src/events.js
+++ src/events.js
@@ -5,138 +5,138 @@ const moveHooks = ['enter', 'leave'];
 const hooks = clickHooks.concat(moveHooks);
 
 export function updateListeners(chart, state, options) {
-	const annotations = options.annotations || [];
-	state.listened = false;
-	state.moveListened = false;
+  const annotations = options.annotations || [];
+  state.listened = false;
+  state.moveListened = false;
 
-	hooks.forEach(hook => {
-		if (typeof options[hook] === 'function') {
-			state.listened = true;
-			state.listeners[hook] = options[hook];
-		}
-	});
-	moveHooks.forEach(hook => {
-		if (typeof options[hook] === 'function') {
-			state.moveListened = true;
-		}
-	});
+  hooks.forEach(hook => {
+    if (typeof options[hook] === 'function') {
+      state.listened = true;
+      state.listeners[hook] = options[hook];
+    }
+  });
+  moveHooks.forEach(hook => {
+    if (typeof options[hook] === 'function') {
+      state.moveListened = true;
+    }
+  });
 
-	if (!state.listened || !state.moveListened) {
-		annotations.forEach(scope => {
-			if (!state.listened) {
-				clickHooks.forEach(hook => {
-					if (typeof scope[hook] === 'function') {
-						state.listened = true;
-					}
-				});
-			}
-			if (!state.moveListened) {
-				moveHooks.forEach(hook => {
-					if (typeof scope[hook] === 'function') {
-						state.listened = true;
-						state.moveListened = true;
-					}
-				});
-			}
-		});
-	}
+  if (!state.listened || !state.moveListened) {
+    annotations.forEach(scope => {
+      if (!state.listened) {
+        clickHooks.forEach(hook => {
+          if (typeof scope[hook] === 'function') {
+            state.listened = true;
+          }
+        });
+      }
+      if (!state.moveListened) {
+        moveHooks.forEach(hook => {
+          if (typeof scope[hook] === 'function') {
+            state.listened = true;
+            state.moveListened = true;
+          }
+        });
+      }
+    });
+  }
 }
 
 export function handleEvent(chart, state, event, options) {
-	if (state.listened) {
-		switch (event.type) {
-		case 'mousemove':
-		case 'mouseout':
-			handleMoveEvents(chart, state, event);
-			break;
-		case 'click':
-			handleClickEvents(chart, state, event, options);
-			break;
-		default:
-		}
-	}
+  if (state.listened) {
+    switch (event.type) {
+    case 'mousemove':
+    case 'mouseout':
+      handleMoveEvents(chart, state, event);
+      break;
+    case 'click':
+      handleClickEvents(chart, state, event, options);
+      break;
+    default:
+    }
+  }
 }
 
 function handleMoveEvents(chart, state, event) {
-	if (!state.moveListened) {
-		return;
-	}
+  if (!state.moveListened) {
+    return;
+  }
 
-	let element;
+  let element;
 
-	if (event.type === 'mousemove') {
-		element = getNearestItem(state.elements, event);
-	}
+  if (event.type === 'mousemove') {
+    element = getNearestItem(state.elements, event);
+  }
 
-	const previous = state.hovered;
-	state.hovered = element;
+  const previous = state.hovered;
+  state.hovered = element;
 
-	dispatchMoveEvents(chart, state, previous, element);
+  dispatchMoveEvents(chart, state, previous, element);
 }
 
 function dispatchMoveEvents(chart, state, previous, element) {
-	if (previous && previous !== element) {
-		dispatchEvent(chart, state, previous.options.leave || state.listeners.leave, previous);
-	}
-	if (element && element !== previous) {
-		dispatchEvent(chart, state, element.options.enter || state.listeners.enter, element);
-	}
+  if (previous && previous !== element) {
+    dispatchEvent(chart, state, previous.options.leave || state.listeners.leave, previous);
+  }
+  if (element && element !== previous) {
+    dispatchEvent(chart, state, element.options.enter || state.listeners.enter, element);
+  }
 }
 
 function handleClickEvents(chart, state, event, options) {
-	const listeners = state.listeners;
-	const element = getNearestItem(state.elements, event);
-	if (element) {
-		const elOpts = element.options;
-		const dblclick = elOpts.dblclick || listeners.dblclick;
-		const click = elOpts.click || listeners.click;
-		if (element.clickTimeout) {
-			// 2nd click before timeout, so its a double click
-			clearTimeout(element.clickTimeout);
-			delete element.clickTimeout;
-			dispatchEvent(chart, state, dblclick, element);
-		} else if (dblclick) {
-			// if there is a dblclick handler, wait for dblClickSpeed ms before deciding its a click
-			element.clickTimeout = setTimeout(() => {
-				delete element.clickTimeout;
-				dispatchEvent(chart, state, click, element);
-			}, options.dblClickSpeed);
-		} else {
-			// no double click handler, just call the click handler directly
-			dispatchEvent(chart, state, click, element);
-		}
-	}
+  const listeners = state.listeners;
+  const element = getNearestItem(state.elements, event);
+  if (element) {
+    const elOpts = element.options;
+    const dblclick = elOpts.dblclick || listeners.dblclick;
+    const click = elOpts.click || listeners.click;
+    if (element.clickTimeout) {
+      // 2nd click before timeout, so its a double click
+      clearTimeout(element.clickTimeout);
+      delete element.clickTimeout;
+      dispatchEvent(chart, state, dblclick, element);
+    } else if (dblclick) {
+      // if there is a dblclick handler, wait for dblClickSpeed ms before deciding its a click
+      element.clickTimeout = setTimeout(() => {
+        delete element.clickTimeout;
+        dispatchEvent(chart, state, click, element);
+      }, options.dblClickSpeed);
+    } else {
+      // no double click handler, just call the click handler directly
+      dispatchEvent(chart, state, click, element);
+    }
+  }
 }
 
 function dispatchEvent(chart, _state, handler, element) {
-	callHandler(handler, [{chart, element}]);
+  callHandler(handler, [{chart, element}]);
 }
 
 function getNearestItem(elements, position) {
-	let minDistance = Number.POSITIVE_INFINITY;
+  let minDistance = Number.POSITIVE_INFINITY;
 
-	return elements
-		.filter((element) => element.inRange(position.x, position.y))
-		.reduce((nearestItems, element) => {
-			const center = element.getCenterPoint();
-			const distance = distanceBetweenPoints(position, center);
+  return elements
+    .filter((element) => element.inRange(position.x, position.y))
+    .reduce((nearestItems, element) => {
+      const center = element.getCenterPoint();
+      const distance = distanceBetweenPoints(position, center);
 
-			if (distance < minDistance) {
-				nearestItems = [element];
-				minDistance = distance;
-			} else if (distance === minDistance) {
-				// Can have multiple items at the same distance in which case we sort by size
-				nearestItems.push(element);
-			}
+      if (distance < minDistance) {
+        nearestItems = [element];
+        minDistance = distance;
+      } else if (distance === minDistance) {
+        // Can have multiple items at the same distance in which case we sort by size
+        nearestItems.push(element);
+      }
 
-			return nearestItems;
-		}, [])
-		.sort((a, b) => {
-			// If there are multiple elements equally close,
-			// sort them by size, then by index
-			const sizeA = a.getArea();
-			const sizeB = b.getArea();
-			return (sizeA > sizeB || sizeA < sizeB) ? sizeA - sizeB : a._index - b._index;
-		})
-		.slice(0, 1)[0]; // return only the top item
+      return nearestItems;
+    }, [])
+    .sort((a, b) => {
+      // If there are multiple elements equally close,
+      // sort them by size, then by index
+      const sizeA = a.getArea();
+      const sizeB = b.getArea();
+      return (sizeA > sizeB || sizeA < sizeB) ? sizeA - sizeB : a._index - b._index;
+    })
+    .slice(0, 1)[0]; // return only the top item
 }
