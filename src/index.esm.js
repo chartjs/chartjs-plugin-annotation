0e6b4ee0758276a5d79c2affacd6875654796233
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-01-14 16:58:36 +0200

    Auto register/unregister annotation types

--- src/index.esm.js
+++ src/index.esm.js
@@ -1,5 +1,1 @@
-export {default as Annotation} from './annotation';
-export {default as BoxAnnotation} from './types/box';
-export {default as LineAnnotation} from './types/line';
-export {default as EllipseAnnotation} from './types/ellipse';
-export {default as PointAnnotation} from './types/point';
+export {default} from './annotation';
