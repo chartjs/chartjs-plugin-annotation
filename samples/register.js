a7a556115b79972ebcf94a72a10e1e2c19871bb9
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-03-15 16:54:23 +0200

    Interactive online samples using VuePress (#356)
    * Samples using vuepress
    
    * Update linter

---
+++ samples/register.js
@@ -0,0 +1,6 @@
+import {Chart, registerables} from 'chart.js';
+import autocolors from 'chartjs-plugin-autocolors';
+import plugin from '../dist/chartjs-plugin-annotation.esm.js';
+
+Chart.register(...registerables);
+Chart.register(plugin, autocolors);
---
+++ samples/utils.js
@@ -0,0 +1,45 @@
+import {valueOrDefault} from 'chart.js/helpers';
+
+// Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+var _seed = Date.now();
+
+export function srand(seed) {
+  _seed = seed;
+}
+
+export function rand(min, max) {
+  min = valueOrDefault(min, 0);
+  max = valueOrDefault(max, 0);
+  _seed = (_seed * 9301 + 49297) % 233280;
+  return min + (_seed / 233280) * (max - min);
+}
+
+export function numbers(config) {
+  var cfg = config || {};
+  var min = valueOrDefault(cfg.min, 0);
+  var max = valueOrDefault(cfg.max, 100);
+  var from = valueOrDefault(cfg.from, []);
+  var count = valueOrDefault(cfg.count, 8);
+  var decimals = valueOrDefault(cfg.decimals, 8);
+  var continuity = valueOrDefault(cfg.continuity, 1);
+  var dfactor = Math.pow(10, decimals) || 0;
+  var data = [];
+  var i, value;
+
+  for (i = 0; i < count; ++i) {
+    value = (from[i] || 0) + this.rand(min, max);
+    if (this.rand() <= continuity) {
+      data.push(Math.round(dfactor * value) / dfactor);
+    } else {
+      data.push(null);
+    }
+  }
+
+  return data;
+}
+
+export function points(config) {
+  const xs = this.numbers(config);
+  const ys = this.numbers(config);
+  return xs.map((x, i) => ({x, y: ys[i]}));
+}
