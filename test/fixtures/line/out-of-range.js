0db8c4403b49241d946bee454f63c519189dd435
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-03-27 23:21:46 +0200

    Skip annotations outside chartArea

---
+++ test/fixtures/line/out-of-range.js
@@ -0,0 +1,40 @@
+module.exports = {
+  config: {
+    type: 'line',
+    data: {
+      datasets: [{
+        data: [10, 20, 30, 0, 55],
+      }],
+      labels: ['A', 'B', 'C', 'D', 'E']
+    },
+    options: {
+      scales: {
+        x: {
+          min: 'C'
+        }
+      },
+      plugins: {
+        legend: false,
+        annotation: {
+          annotations: {
+            annotation1: {
+              type: 'line',
+              scaleID: 'x',
+              borderWidth: 3,
+              borderColor: 'black',
+              value: 'B',
+              label: {
+                backgroundColor: 'red',
+                content: 'shold not be drawn',
+                enabled: true
+              }
+            }
+          }
+        }
+      },
+    }
+  },
+  options: {
+    spriteText: true,
+  }
+};
