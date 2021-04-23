5febbbbb25b7fadc91ea2d0fc5c68c4ef7965000
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-04-23 21:08:28 +0300

    Add fixtures for cornerRadius (#417)

---
+++ test/fixtures/box/cornerRadius.js
@@ -0,0 +1,63 @@
+module.exports = {
+  config: {
+    type: 'bar',
+    options: {
+      scales: {
+        x: {
+          display: false,
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        },
+        y: {
+          display: false,
+          min: 0,
+          max: 25
+        }
+      },
+      plugins: {
+        annotation: {
+          annotations: {
+            box1: {
+              type: 'box',
+              xScaleID: 'x',
+              yScaleID: 'y',
+              xMin: 1.5,
+              xMax: 3.5,
+              yMin: 5,
+              yMax: 10,
+              backgroundColor: 'rgba(33, 101, 171, 0.5)',
+              borderColor: 'rgb(33, 101, 171)',
+              cornerRadius: 8,
+              borderWidth: 5,
+            },
+            box2: {
+              type: 'box',
+              xScaleID: 'x',
+              yScaleID: 'y',
+              xMin: 'July',
+              xMax: 'May',
+              yMin: 11,
+              yMax: 15,
+              backgroundColor: 'rgba(101, 33, 171, 0.5)',
+              borderColor: 'rgb(101, 33, 171)',
+              cornerRadius: Infinity,
+              borderWidth: 5,
+            },
+            box3: {
+              type: 'box',
+              xScaleID: 'x',
+              yScaleID: 'y',
+              xMin: 0,
+              xMax: 'May',
+              yMin: 20,
+              yMax: 16,
+              backgroundColor: 'rgba(171, 101, 33, 0.5)',
+              borderColor: 'rgb(171, 101, 33)',
+              cornerRadius: 4,
+              borderWidth: 5,
+            }
+          }
+        }
+      }
+    }
+  }
+};
