614bf93eb0a53af0f14de1f4fc0fe2d512329c44
elias.stocker <elias.stocker@confer.ch>
2021-11-08 13:59:50 +0100

    Typings for default settings on element

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,17 @@
-import { Plugin, ChartType } from 'chart.js';
-import { AnnotationPluginOptions } from './options';
+import { ChartType, Plugin } from 'chart.js';
+import { AnnotationPluginOptions, BoxAnnotationOptions, EllipseAnnotationOptions, LineAnnotationOptions, PointAnnotationOptions } from './options';
 
 declare module 'chart.js' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface PluginOptionsByType<TType extends ChartType> {
     annotation: AnnotationPluginOptions;
+  }
+
+  interface ElementOptionsByType<TType extends ChartType> {
+    boxAnnotation: BoxAnnotationOptions;
+    lineAnnotation: LineAnnotationOptions;
+    ellipseAnnotation: EllipseAnnotationOptions;
+    pointAnnotation: PointAnnotationOptions;
   }
 }
 
