2413a6bb68d375524fdb0a525f2d68e2111d7298
stockiNail <stocki.nail@gmail.com>
2021-11-09 13:48:32 +0100

    adds test case

---
+++ test/fixtures/box/label.js
@@ -0,0 +1,85 @@
+module.exports = {
+  tolerance: 0.0055,
+  config: {
+    type: 'bar',
+    options: {
+      scales: {
+        x: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        },
+        y: {
+          display: false,
+          min: 0,
+          max: 25
+        }
+      },
+      plugins: {
+        annotation: {
+          annotations: {
+            box1: {
+              type: 'box',
+              xScaleID: 'x',
+              yScaleID: 'y',
+              xMin: 1.5,
+              xMax: 3.5,
+              yMin: 5,
+              yMax: 10,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderDash: [6, 6],
+              borderWidth: 5,
+              label: {
+                enabled: true,
+                content: 'This is a label',
+              }
+            },
+            box2: {
+              type: 'box',
+              xScaleID: 'x',
+              yScaleID: 'y',
+              xMin: 'May',
+              xMax: 'July',
+              yMin: 11,
+              yMax: 15,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderDash: [6, 6],
+              borderWidth: 5,
+              label: {
+                enabled: true,
+                content: 'This is a label',
+                textAlign: 'left',
+                position: 'top'
+              }
+            },
+            box3: {
+              type: 'box',
+              xScaleID: 'x',
+              yScaleID: 'y',
+              xMin: -0.5,
+              xMax: 'May',
+              yMin: 16,
+              yMax: 20,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderDash: [6, 6],
+              borderWidth: 5,
+              label: {
+                enabled: true,
+                content: (ctx) => ['This is a label', 'created by callback, type ' + ctx.type],
+                font: {
+                  size: 18
+                },
+                textAlign: 'right',
+                position: 'bottom'
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  options: {
+    spriteText: true
+  }
+};
