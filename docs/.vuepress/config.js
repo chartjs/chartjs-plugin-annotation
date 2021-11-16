84462eaa54a6592f05076a3dbf743d8ba81a11c0
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-11-16 19:22:39 +0200

    Documentation versioning (#492)
    * Documentation versioning
    
    * master instead of $VERSION

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,61 @@
+const docsVersion = "VERSION";
+const base = process.env.NODE_ENV === "development" ? '/chartjs-plugin-annotation/master/' : `/chartjs-plugin-annotation/${docsVersion}/`;
+
 module.exports = {
   dest: 'dist/docs',
   title: 'chartjs-plugin-annotation',
   description: 'Annotations for Chart.js',
   theme: 'chartjs',
-  base: '/chartjs-plugin-annotation/',
+  base,
   head: [
     ['link', {rel: 'icon', href: '/favicon.png'}],
   ],
   plugins: [
+    ['flexsearch'],
     ['redirect', {
       redirectors: [
         // Default sample page when accessing /samples.
         {base: '/samples', alternative: ['types/line']},
       ],
+    }],
+    ['@simonbrunel/vuepress-plugin-versions', {
+      filters: {
+        suffix: (tag) => tag ? ` (${tag})` : '',
+        title: (v, vars) => window.location.href.includes('master') ? 'Development (master)' : v + (vars.tag ? ` (${tag})` : ''),
+      },
+      menu: {
+        text: '{{version|title}}',
+        items: [
+          {
+            text: 'Documentation',
+            items: [
+              {
+                text: 'Development (master)',
+                link: '/chartjs-plugin-annotation/master/',
+              },
+              {
+                type: 'versions',
+                text: '{{version}}{{tag|suffix}}',
+                link: '/chartjs-plugin-annotation/{{version}}/',
+                exclude: /^[0]\.[0-4]\./,
+                group: 'minor',
+              }
+            ]
+          },
+          {
+            text: 'Release notes (5 latest)',
+            items: [
+              {
+                type: 'versions',
+                limit: 5,
+                target: '_blank',
+                group: 'patch',
+                link: 'https://github.com/chartjs/chartjs-plugin-annotation/releases/tag/v{{version}}'
+              }
+            ]
+          }
+        ]
+      },
     }],
   ],
   themeConfig: {
---
+++ scripts/docs-config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# tag is next|latest|master|x.x.x
+# https://www.chartjs.org/chartjs-pligin-annotation/$tag/
+function tag_from_version {
+    local version=$1
+    local mode=$2
+    local tag=''
+    if [ "$version" == "master" ]; then
+        tag=master
+    elif [[ "$version" =~ ^[^-]+$ ]]; then
+      if [[ "$mode" == "release" ]]; then
+        tag=$version
+      else
+        tag=latest
+      fi
+    else
+        tag=next
+    fi
+    echo $tag
+}
+
+VERSION=$1
+MODE=$2
+
+TAG=$(tag_from_version "$VERSION" "$MODE")
+
+sed -i -e "s/VERSION/$TAG/g" "docs/.vuepress/config.js"
