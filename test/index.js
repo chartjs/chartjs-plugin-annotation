cd38c2364e7aef30a481e89b6e971b7a3a3e89f0
stockiNail <stocki.nail@gmail.com>
2021-12-02 13:53:26 +0100

    changes test cases specs adding common events tests

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ window.devicePixelRatio = 1;
 window.acquireChart = acquireChart;
 window.afterEvent = afterEvent;
 window.triggerMouseEvent = triggerMouseEvent;
+window.testEvents = testEvents;
 
 jasmine.fixtures = specsFromFixtures;
 
@@ -14,3 +15,90 @@ beforeEach(function() {
 afterEach(function() {
   releaseCharts();
 });
+
+function testEvents(options, innerNode) {
+  const descr = innerNode ? options.type + '.' + innerNode : options.type;
+  const chartOptions = {
+    type: 'scatter',
+    options: {
+      animation: false,
+      scales: {
+        x: {
+          display: false,
+          min: 0,
+          max: 10
+        },
+        y: {
+          display: false,
+          min: 0,
+          max: 10
+        }
+      },
+      plugins: {
+        legend: false,
+        annotation: {
+        }
+      }
+    },
+  };
+
+  describe('events', function() {
+
+    it(`should detect enter and click events on ${descr}`, function(done) {
+      const enterSpy = jasmine.createSpy('enter');
+      const clickSpy = jasmine.createSpy('click');
+
+      options.enter = enterSpy;
+      options.click = clickSpy;
+      chartOptions.options.plugins.annotation.annotations = [options];
+
+      const chart = window.acquireChart(chartOptions);
+      const eventPoint = getEventPoint(chart, innerNode);
+
+      window.triggerMouseEvent(chart, 'mousemove', eventPoint);
+      window.afterEvent(chart, 'mousemove', function() {
+        expect(enterSpy.calls.count()).toBe(1);
+        window.afterEvent(chart, 'click', function() {
+          expect(clickSpy.calls.count()).toBe(1);
+          delete options.enter;
+          delete options.click;
+          done();
+        });
+        window.triggerMouseEvent(chart, 'click', eventPoint);
+      });
+    });
+
+    it(`should detect dbl click on ${descr}`, function(done) {
+      const dblClickSpy = jasmine.createSpy('dblclick');
+
+      options.dblclick = dblClickSpy;
+      chartOptions.options.plugins.annotation.dblClickSpeed = 1000;
+      chartOptions.options.plugins.annotation.annotations = [options];
+
+      const chart = window.acquireChart(chartOptions);
+      const eventPoint = getEventPoint(chart, innerNode);
+      
+      let dblClick = false;
+      window.afterEvent(chart, 'click', function() {
+        if (!dblClick) {
+          dblClick = true;
+          window.triggerMouseEvent(chart, 'click', eventPoint);
+        } else {
+          expect(dblClickSpy.calls.count()).toBe(1);
+          delete options.dblclick;
+          done();
+        }
+      });
+      window.triggerMouseEvent(chart, 'click', eventPoint);
+    });
+  });
+
+}
+
+function getEventPoint(chart, innerNode) {
+  const Annotation = window['chartjs-plugin-annotation'];
+  const state = Annotation._getState(chart);
+  const annotation = state.elements[0];
+  const element = innerNode ? annotation[innerNode] : annotation;
+  return innerNode ? {x: element.x + element.width / 2, y: element.y} : element.getCenterPoint();
+}
--- test/specs/line.spec.js
+++ test/specs/line.spec.js
@@ -1,3 +1,41 @@
 describe('Line annotation', function() {
   describe('auto', jasmine.fixtures('line'));
+  const options = {
+    type: 'line',
+    id: 'test',
+    scaleID: 'x',
+    value: 2,
+    borderWidth: 6,
+    label: {
+      enabled: true,
+      content: 'This is my label',
+      rotation: 0
+    }
+  };
+
+  window.testEvents(options);
+
+  window.testEvents(options, 'labelRect');
+
+  const optionsLimit = {
+    type: 'line',
+    id: 'test',
+    xScaleID: 'x',
+    yScaleID: 'y',
+    xMin: 0,
+    yMin: 2,
+    xMax: 10,
+    yMax: 2,
+    borderWidth: 6,
+    label: {
+      enabled: true,
+      content: 'This is my label',
+      rotation: 0
+    }
+  };
+
+  window.testEvents(optionsLimit);
+
+  window.testEvents(optionsLimit, 'labelRect');
+
 });
