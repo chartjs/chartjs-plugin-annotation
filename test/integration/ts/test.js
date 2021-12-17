8f43b3c95318303080c9bfbcebecbfdfa902fc7b
Jukka Kurkela <jukka.kurkela@gmail.com>
2021-12-17 14:57:54 +0200

    Add integration tests (ts) (#569)
    * Add integration tests (ts)
    
    * increase timeout

---
+++ test/integration/ts/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const path = require('path');
+const childProcess = require('child_process');
+
+const {dependencies} = require('./package.json');
+
+const tsVersions = Object.keys(dependencies)
+  .filter((pkg) => pkg.startsWith('typescript-'))
+  .sort((a, b) => b.localeCompare(a));
+
+for (const version of tsVersions) {
+  console.log(`Testing on ${version} ...`);
+  childProcess.execSync('node ' + tscPath(version), {stdio: 'inherit'});
+}
+
+function tscPath(version) {
+  return path.join(__dirname, 'node_modules', version, 'bin/tsc');
+}
