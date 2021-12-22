43ea6d0736a852468082b513ec9adff65d2981f7
stockiNail <stocki.nail@gmail.com>
2021-12-22 09:04:12 +0100

    Fix events catching on rotated ellipse annotations (#583)
    * Fix events catching on rotated ellipse annotations
    
    * adds specific test for rotated ellipse

--- test/specs/ellipse.spec.js
+++ test/specs/ellipse.spec.js
@@ -1,7 +1,7 @@
 describe('Ellipse annotation', function() {
   describe('auto', jasmine.fixtures('ellipse'));
 
-  window.testEvents({
+  const options = {
     type: 'ellipse',
     id: 'test',
     xScaleID: 'x',
@@ -11,6 +11,90 @@ describe('Ellipse annotation', function() {
     xMax: 4,
     yMax: 4,
     borderWidth: 0
+  };
+
+  window.testEvents(options);
+
+  options.rotation = 90;
+
+  window.testEvents(options);
+
+  describe('events on rotated ellipsed', function() {
+
+    const chartConfig = {
+      type: 'scatter',
+      options: {
+        animation: false,
+        scales: {
+          x: {
+            display: false,
+            min: 0,
+            max: 10
+          },
+          y: {
+            display: false,
+            min: 0,
+            max: 10
+          }
+        },
+        plugins: {
+          legend: false,
+          annotation: {
+            annotations: {
+              ellipse: {
+                type: 'ellipse',
+                xScaleID: 'x',
+                yScaleID: 'y',
+                xMin: 2,
+                yMin: 4,
+                xMax: 8,
+                yMax: 6,
+                rotation: 45
+              }
+            }
+          }
+        }
+      },
+    };
+
+    const ellipseOpts = chartConfig.options.plugins.annotation.annotations.ellipse;
+
+    it('should detect click event', function(done) {
+      const clickSpy = jasmine.createSpy('click');
+
+      ellipseOpts.click = clickSpy;
+
+      const chart = window.acquireChart(chartConfig);
+      const xScale = chart.scales.x;
+      const yScale = chart.scales.y;
+      const eventPoint = {x: xScale.getPixelForValue(4), y: yScale.getPixelForValue(7)};
+
+      window.afterEvent(chart, 'click', function() {
+        expect(clickSpy.calls.count()).toBe(1);
+        delete ellipseOpts.click;
+        done();
+      });
+      window.triggerMouseEvent(chart, 'click', eventPoint);
+    });
+
+    it('should not detect click event', function(done) {
+      const clickSpy = jasmine.createSpy('click');
+
+      ellipseOpts.click = clickSpy;
+
+      const chart = window.acquireChart(chartConfig);
+      const xScale = chart.scales.x;
+      const yScale = chart.scales.y;
+      const eventPoint = {x: xScale.getPixelForValue(3), y: yScale.getPixelForValue(5)};
+
+      window.afterEvent(chart, 'click', function() {
+        expect(clickSpy.calls.count()).toBe(0);
+        delete ellipseOpts.click;
+        done();
+      });
+      window.triggerMouseEvent(chart, 'click', eventPoint);
+    });
+
   });
 
 });
