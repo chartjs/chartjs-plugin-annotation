b83ff4a4075943063ed71034f25342891775fef7
stockiNail <stocki.nail@gmail.com>
2021-12-24 15:26:06 +0100

    Consider the line thickness in the intersect calculation

--- test/specs/line.spec.js
+++ test/specs/line.spec.js
@@ -39,4 +39,77 @@ describe('Line annotation', function() {
 
   window.testEvents(optionsLimit, 'labelRect');
 
+  describe('events on line with thickness', function() {
+
+    const chartConfig = {
+      type: 'scatter',
+      options: {
+        animation: false,
+        scales: {
+          x: {
+            display: false,
+            min: 0,
+            max: 10
+          },
+          y: {
+            display: false,
+            min: 0,
+            max: 10
+          }
+        },
+        plugins: {
+          legend: false,
+          annotation: {
+            annotations: {
+              line: {
+                type: 'line',
+                scaleID: 'y',
+                value: 5,
+                borderWidth: 60
+              }
+            }
+          }
+        }
+      },
+    };
+
+    const lineOpts = chartConfig.options.plugins.annotation.annotations.line;
+
+    it('should detect click event, considering the tickness on bottom', function(done) {
+      const clickSpy = jasmine.createSpy('click');
+
+      lineOpts.click = clickSpy;
+
+      const chart = window.acquireChart(chartConfig);
+      const xScale = chart.scales.x;
+      const yScale = chart.scales.y;
+      const eventPoint = {x: xScale.getPixelForValue(5), y: yScale.getPixelForValue(lineOpts.value) + lineOpts.borderWidth / 2 - 2};
+
+      window.afterEvent(chart, 'click', function() {
+        expect(clickSpy.calls.count()).toBe(1);
+        delete lineOpts.click;
+        done();
+      });
+      window.triggerMouseEvent(chart, 'click', eventPoint);
+    });
+
+    it('should detect click event, considering the tickness on top', function(done) {
+      const clickSpy = jasmine.createSpy('click');
+
+      lineOpts.click = clickSpy;
+
+      const chart = window.acquireChart(chartConfig);
+      const xScale = chart.scales.x;
+      const yScale = chart.scales.y;
+      const eventPoint = {x: xScale.getPixelForValue(5), y: yScale.getPixelForValue(lineOpts.value) - lineOpts.borderWidth / 2 + 2};
+
+      window.afterEvent(chart, 'click', function() {
+        expect(clickSpy.calls.count()).toBe(1);
+        delete lineOpts.click;
+        done();
+      });
+      window.triggerMouseEvent(chart, 'click', eventPoint);
+    });
+
+  });
 });
