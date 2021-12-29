2aab524d56c1b7e44bdb0cdce1b4bc2b910c59fe
stockiNail <stocki.nail@gmail.com>
2021-12-29 11:18:07 +0100

    Properly consider the line thickness in the intersect calculation (#591)
    * Consider the line thickness in the intersect calculation
    
    * fixes similar code
    
    * apply review
    
    * remove breaking change

--- test/specs/line.spec.js
+++ test/specs/line.spec.js
@@ -39,4 +39,80 @@ describe('Line annotation', function() {
 
   window.testEvents(optionsLimit, 'labelRect');
 
+  describe('events on line with thickness', function() {
+
+    const chartConfig = {
+      type: 'scatter',
+      options: {
+        animation: false,
+        scales: {
+          x: {
+            display: false,
+            min: 0,
+            max: 10
+          },
+          y: {
+            display: false,
+            min: 0,
+            max: 10
+          }
+        },
+        plugins: {
+          legend: false,
+          annotation: {
+            annotations: {
+              line: {
+                type: 'line',
+                scaleID: 'y',
+                value: 5
+              }
+            }
+          }
+        }
+      },
+    };
+
+    const lineOpts = chartConfig.options.plugins.annotation.annotations.line;
+
+    for (let i = 2; i <= 60; i++) {
+
+      lineOpts.borderWidth = i;
+
+      it('should detect click event, considering the tickness on bottom', function(done) {
+        const clickSpy = jasmine.createSpy('click');
+
+        lineOpts.click = clickSpy;
+
+        const chart = window.acquireChart(chartConfig);
+        const xScale = chart.scales.x;
+        const yScale = chart.scales.y;
+        const eventPoint = {x: xScale.getPixelForValue(5), y: yScale.getPixelForValue(lineOpts.value) + lineOpts.borderWidth / 2 - 1};
+
+        window.afterEvent(chart, 'click', function() {
+          expect(clickSpy.calls.count()).toBe(1);
+          delete lineOpts.click;
+          done();
+        });
+        window.triggerMouseEvent(chart, 'click', eventPoint);
+      });
+
+      it('should detect click event, considering the tickness on top', function(done) {
+        const clickSpy = jasmine.createSpy('click');
+
+        lineOpts.click = clickSpy;
+
+        const chart = window.acquireChart(chartConfig);
+        const xScale = chart.scales.x;
+        const yScale = chart.scales.y;
+        const eventPoint = {x: xScale.getPixelForValue(5), y: yScale.getPixelForValue(lineOpts.value) - lineOpts.borderWidth / 2 + 1};
+
+        window.afterEvent(chart, 'click', function() {
+          expect(clickSpy.calls.count()).toBe(1);
+          delete lineOpts.click;
+          done();
+        });
+        window.triggerMouseEvent(chart, 'click', eventPoint);
+      });
+    }
+  });
 });
