3d837ea859dfbeaf0f1cbf9bdd9fcd248040c739
stockiNail <stocki.nail@gmail.com>
2021-12-30 15:08:16 +0100

    Fixes events triggering on the label of a line annotation

--- test/specs/line.spec.js
+++ test/specs/line.spec.js
@@ -39,6 +39,96 @@ describe('Line annotation', function() {
 
   window.testEvents(optionsLimit, 'labelRect');
 
+  describe('events on line label', function() {
+
+    const chartConfig = {
+      type: 'scatter',
+      options: {
+        animation: false,
+        scales: {
+          x: {
+            display: false,
+            min: 0,
+            max: 10
+          },
+          y: {
+            display: false,
+            min: 0,
+            max: 10
+          }
+        },
+        plugins: {
+          legend: false,
+          annotation: {
+            annotations: {
+              line: {
+                type: 'line',
+                scaleID: 'y',
+                value: 5,
+                borderWidth: 1,
+                label: {
+                  enabled: true,
+                  content: 'This is my label',
+                  rotation: 0
+                }
+              }
+            }
+          }
+        }
+      },
+    };
+
+    const lineOpts = chartConfig.options.plugins.annotation.annotations.line;
+
+    it('should detect enter and leave events only on the label', function(done) {
+      const enterSpy = jasmine.createSpy('enter');
+      const leaveSpy = jasmine.createSpy('leave');
+
+      lineOpts.enter = enterSpy;
+      lineOpts.leave = leaveSpy;
+
+      const chart = window.acquireChart(chartConfig);
+      const x = chart.chartArea.width / 2;
+      const yScale = chart.scales.y;
+      const eventPoint = {x, y: yScale.getPixelForValue(lineOpts.value) + 10};
+
+      window.triggerMouseEvent(chart, 'mousemove', eventPoint);
+      window.afterEvent(chart, 'mousemove', function() {
+        expect(enterSpy.calls.count()).toBe(1);
+
+        window.triggerMouseEvent(chart, 'mousemove', {
+          x: 0,
+          y: 0
+        });
+
+        window.afterEvent(chart, 'mousemove', function() {
+          expect(leaveSpy.calls.count()).toBe(1);
+          delete lineOpts.enter;
+          delete lineOpts.leave;
+          done();
+        });
+      });
+    });
+
+    it('should detect click event only on the label', function(done) {
+      const clickSpy = jasmine.createSpy('click');
+
+      lineOpts.click = clickSpy;
+
+      const chart = window.acquireChart(chartConfig);
+      const x = chart.chartArea.width / 2;
+      const yScale = chart.scales.y;
+      const eventPoint = {x, y: yScale.getPixelForValue(lineOpts.value) + 10};
+
+      window.afterEvent(chart, 'click', function() {
+        expect(clickSpy.calls.count()).toBe(1);
+        delete lineOpts.click;
+        done();
+      });
+      window.triggerMouseEvent(chart, 'click', eventPoint);
+    });
+  });
+
   describe('events on line with thickness', function() {
 
     const chartConfig = {
