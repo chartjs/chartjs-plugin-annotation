7a87cbb68ea661343ba4b7bcc1e838b1cc96db62
stockiNail <stocki.nail@gmail.com>
2022-01-15 20:25:01 +0100

    Fix the scale limit update when annotation value is 0 (#627)

---
+++ test/fixtures/line/out-of-range-value0.js
@@ -0,0 +1,35 @@
+module.exports = {
+  config: {
+    type: 'line',
+    data: {
+      datasets: [{
+        data: [40, 20, 30, 60, 55],
+      }],
+      labels: ['A', 'B', 'C', 'D', 'E']
+    },
+    options: {
+      plugins: {
+        legend: false,
+        annotation: {
+          annotations: {
+            annotation1: {
+              type: 'line',
+              scaleID: 'y',
+              borderWidth: 3,
+              borderColor: 'black',
+              value: 0,
+              label: {
+                backgroundColor: 'red',
+                content: 'should be drawn',
+                enabled: true
+              }
+            }
+          }
+        }
+      },
+    }
+  },
+  options: {
+    spriteText: true,
+  }
+};
