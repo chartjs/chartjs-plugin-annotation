f8bf81092d2479bf17dc0b71acea2559486437bc
stockiNail <stocki.nail@gmail.com>
2022-01-19 15:42:01 +0100

    Label fixtures: merge position tests in vertical and horizontal ones (#643)
    * Label fixtures: remove position tests with percentage or points
    
    * changes max of position vertical chart scale
    
    * adds 0%, 100% and invalid position cases

--- test/fixtures/label/positionVertical.js
+++ test/fixtures/label/positionVertical.js
@@ -7,50 +7,129 @@ module.exports = {
         {
           borderWidth: 1,
           borderColor: 'red',
-          data: [20, 20, 20, 20, 20, 20, 20]
-        }
+          data: [8, 8, 8, 8, 8, 8, 8]
+        },
+        {
+          borderWidth: 1,
+          borderColor: 'red',
+          data: [16, 16, 16, 16, 16, 16, 16]
+        },
       ]
     },
     options: {
       scales: {
         x: {
+          display: false,
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         },
         y: {
           display: false,
           min: 0,
-          max: 25
+          max: 24
         }
       },
       plugins: {
         legend: false,
         annotation: {
           annotations: {
-            text1: {
+            center: {
               type: 'label',
-              xValue: 'February',
-              yValue: 20,
-              backgroundColor: 'transparent',
-              borderWidth: 0,
-              content: 'position: undefined'
+              xValue: 0.5,
+              yValue: 8,
+              content: ['position', '{y: center}'],
+              position: {
+                y: 'center'
+              }
             },
-            text2: {
+            start: {
               type: 'label',
-              xValue: 'April',
-              yValue: 20,
-              backgroundColor: 'transparent',
-              borderWidth: 0,
-              content: 'position: start',
-              position: 'start'
+              xValue: 2,
+              yValue: 8,
+              content: ['position', '{y: start}'],
+              position: {
+                y: 'start'
+              }
             },
-            text3: {
+            end: {
               type: 'label',
-              xValue: 'June',
-              yValue: 20,
-              backgroundColor: 'transparent',
-              borderWidth: 0,
-              content: 'position: end',
-              position: 'end'
+              xValue: 3.5,
+              yValue: 8,
+              content: ['position', '{y: end}'],
+              position: {
+                y: 'end'
+              }
+            },
+            invalid: {
+              type: 'label',
+              xValue: 5,
+              yValue: 8,
+              content: ['position', '{y: invalid}'],
+              position: {
+                y: 'invalid'
+              }
+            },
+            perc0: {
+              type: 'label',
+              xValue: 0.3,
+              yValue: 16,
+              content: ['position', '{y: 0%}'],
+              position: {
+                y: '0%'
+              }
+            },
+            perc25: {
+              type: 'label',
+              xValue: 1.3,
+              yValue: 16,
+              content: ['position', '{y: 25%}'],
+              position: {
+                y: '25%'
+              }
+            },
+            perc50: {
+              type: 'label',
+              xValue: 2.3,
+              yValue: 16,
+              content: ['position', '{y: 50%}'],
+              position: {
+                y: '50%'
+              },
+            },
+            perc75: {
+              type: 'label',
+              xValue: 3.3,
+              yValue: 16,
+              content: ['position', '{y: 75%}'],
+              position: {
+                y: '75%'
+              },
+            },
+            perc100: {
+              type: 'label',
+              xValue: 4.3,
+              yValue: 16,
+              content: ['position', '{y: 100%}'],
+              position: {
+                y: '100%'
+              },
+            },
+            percMoreThan100: {
+              type: 'label',
+              xValue: 5.5,
+              yValue: 16,
+              content: ['position', '{y: > 100%}'],
+              position: {
+                y: '850%'
+              },
+            },
+            percLessThan100: {
+              type: 'label',
+              xValue: 5,
+              yValue: 16,
+              content: ['position', '{y: < 0%}'],
+              position: {
+                y: '-850%'
+              },
             }
           }
         }
