4e752842ba7ccc6d5de6f3202c4b7cf627ee18f7
stockiNail <stocki.nail@gmail.com>
2022-01-21 09:51:10 +0100

    apply review

--- test/specs/point.spec.js
+++ test/specs/point.spec.js
@@ -2,54 +2,77 @@ describe('Point annotation', function() {
   describe('auto', jasmine.fixtures('point'));
 
   describe('inRange', function() {
-    const annotation = {
+    const annotation1 = {
       type: 'point',
-      xValue: 5,
-      yValue: 5,
+      xValue: 7,
+      yValue: 7,
       radius: 30
     };
+    const annotation2 = {
+      type: 'point',
+      xValue: 3,
+      yValue: 3,
+      radius: 5
+    };
+    const annotation3 = {
+      type: 'point',
+      xValue: 3,
+      yValue: 3,
+      radius: 0
+    };
 
-    const chart = window.scatter10x10({test: annotation});
-    const element = window.getAnnotationElements(chart)[0];
+    const chart = window.scatter10x10({test1: annotation1, test2: annotation2, test3: annotation3});
+    const elems = window.getAnnotationElements(chart).filter(el => el.options.radius > 0);
+    const elemsNoRad = window.getAnnotationElements(chart).filter(el => el.options.radius === 0);
 
-    it('should return true inside element', function() {
-      for (const borderWidth of [0, 10]) {
-        const halfBorder = borderWidth / 2;
-        element.options.borderWidth = borderWidth;
-        for (const x of [element.x - element.width / 2 - halfBorder, element.x + element.width / 2 + halfBorder]) {
-          expect(element.inRange(x, element.y)).toEqual(true);
+    elems.forEach(function(element) {
+      it(`should return true inside element '${element.options.id}'`, function() {
+        for (const borderWidth of [0, 10]) {
+          const halfBorder = borderWidth / 2;
+          element.options.borderWidth = borderWidth;
+          const radius = element.height / 2;
+          for (const angle of [0, 45, 90, 135, 180, 225, 270, 315]) {
+            const rad = angle * (Math.PI / 180);
+            const {x, y} = {
+              x: element.x + Math.cos(rad) * (radius + halfBorder - 1),
+              y: element.y + Math.sin(rad) * (radius + halfBorder - 1)
+            };
+            expect(element.inRange(x, y)).withContext(`in, angle: ${angle}, radius: ${radius}, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(true);
+          }
         }
-        for (const y of [element.y - element.height / 2 - halfBorder, element.y + element.height / 2 + halfBorder]) {
-          expect(element.inRange(element.x, y)).toEqual(true);
+      });
+
+      it(`should return false outside element '${element.options.id}'`, function() {
+        for (const borderWidth of [0, 10]) {
+          const halfBorder = borderWidth / 2;
+          element.options.borderWidth = borderWidth;
+          const radius = element.height / 2;
+          for (const angle of [0, 45, 90, 135, 180, 225, 270, 315]) {
+            const rad = angle * (Math.PI / 180);
+            const {x, y} = {
+              x: element.x + Math.cos(rad) * (radius + halfBorder + 1),
+              y: element.y + Math.sin(rad) * (radius + halfBorder + 1)
+            };
+            expect(element.inRange(x, y)).withContext(`in, angle: ${angle}, radius: ${radius}, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(false);
+          }
         }
-      }
+      });
     });
 
-    it('should return false outside element', function() {
-      for (const borderWidth of [0, 10]) {
-        const halfBorder = borderWidth / 2;
-        element.options.borderWidth = borderWidth;
-        for (const x of [element.x - element.width / 2 - halfBorder - 1, element.x + element.width / 2 + halfBorder + 1]) {
-          expect(element.inRange(x, element.y)).toEqual(false);
+    elemsNoRad.forEach(function(element) {
+      it(`should return false radius is 0 element '${element.options.id}'`, function() {
+        element.width = 0;
+        for (const borderWidth of [0, 10]) {
+          const halfBorder = borderWidth / 2;
+          element.options.borderWidth = borderWidth;
+          for (const x of [element.x - halfBorder, element.x + halfBorder]) {
+            expect(element.inRange(x, element.y)).withContext(`in, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${element.y.toFixed(1)}}`).toEqual(false);
+          }
+          for (const y of [element.y - halfBorder, element.y + halfBorder]) {
+            expect(element.inRange(element.x, y)).withContext(`in, borderWidth: ${borderWidth}, {x: ${element.x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(false);
+          }
         }
-        for (const y of [element.y - element.height / 2 - halfBorder - 1, element.y + element.height / 2 + halfBorder + 1]) {
-          expect(element.inRange(element.x, y)).toEqual(false);
-        }
-      }
-    });
-
-    it('should return false element radius is 0', function() {
-      element.width = 0;
-      for (const borderWidth of [0, 10]) {
-        const halfBorder = borderWidth / 2;
-        element.options.borderWidth = borderWidth;
-        for (const x of [element.x - halfBorder, element.x + halfBorder]) {
-          expect(element.inRange(x, element.y)).toEqual(false);
-        }
-        for (const y of [element.y - halfBorder, element.y + halfBorder]) {
-          expect(element.inRange(element.x, y)).toEqual(false);
-        }
-      }
+      });
     });
   });
 });
