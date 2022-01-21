deeef97bb37fad6912c8ebfe3822a5fbcd6467dc
stockiNail <stocki.nail@gmail.com>
2022-01-21 14:18:16 +0100

    Polygon: change event tests to inRange tests

--- test/specs/polygon.spec.js
+++ test/specs/polygon.spec.js
@@ -1,91 +1,94 @@
 describe('Polygon annotation', function() {
   describe('auto', jasmine.fixtures('polygon'));
 
-  const eventIn = function(xScale, yScale, element) {
-    const options = element.options;
-    const adjust = options.borderWidth / 2 - 1;
-    return {x: element.x, y: element.y - element.height / 2 - adjust};
-  };
-  const eventOut = function(xScale, yScale, element) {
-    const options = element.options;
-    const adjust = options.borderWidth / 2 + 1;
-    return {x: element.x, y: element.y - element.height / 2 - adjust};
-  };
+  describe('inRange', function() {
+    const annotation1 = {
+      type: 'polygon',
+      xValue: 7,
+      yValue: 7,
+      radius: 30
+    };
+    const annotation2 = {
+      type: 'polygon',
+      xValue: 3,
+      yValue: 3,
+      radius: 0
+    };
+    const annotation3 = {
+      type: 'polygon',
+      xValue: 5,
+      yValue: 5,
+      radius: 0
+    };
 
-  window.testEvents({
-    type: 'polygon',
-    id: 'test',
-    xValue: 5,
-    yValue: 5,
-    rotation: 0,
-    radius: 30
-  }, eventIn, eventOut);
+    const chart = window.scatter10x10({annotation1, annotation2, annotation3});
+    const elems = window.getAnnotationElements(chart).filter(el => el.options.radius > 0);
+    const elemsNoRad = window.getAnnotationElements(chart).filter(el => el.options.radius === 0);
 
-  describe('events, removing borderWidth by callback, ', function() {
-    const chartConfig = {
-      type: 'scatter',
-      options: {
-        animation: false,
-        scales: {
-          x: {
-            display: false,
-            min: 0,
-            max: 10
-          },
-          y: {
-            display: false,
-            min: 0,
-            max: 10
-          }
-        },
-        plugins: {
-          legend: false,
-          annotation: {
-            annotations: {
-              polygon: {
-                type: 'polygon',
-                xValue: 5,
-                yValue: 5,
-                radius: 20,
-                borderWidth: 10
-              }
+    elems.forEach(function(element) {
+      it(`should return true inside element '${element.options.id}'`, function() {
+        const rotation = element.options.rotation;
+        for (let sides = 4; sides <= 4; sides++) {
+          element.options.sides = sides;
+          for (const borderWidth of [0]) {
+            const halfBorder = borderWidth / 2;
+            element.options.borderWidth = borderWidth;
+            const radius = element.height / 2;
+            const angle = (2 * Math.PI) / sides;
+            const vertices = [];
+            let rad = rotation * (Math.PI / 180);
+            for (let i = 0; i < sides; i++, rad += angle) {
+              const sin = Math.sin(rad);
+              const cos = Math.cos(rad);
+              vertices.push({
+                x: element.x + sin * (radius + halfBorder - 1),
+                y: element.y - cos * (radius + halfBorder - 1)
+              });
+            }
+            for (const vertex of vertices) {
+              //console.log('sides: '+ sides + ' borderWidth: ' + borderWidth + ' in: ' + element.inRange(vertex.x, vertex.y));
+              console.log({x: vertex.x, y: vertex.y}, {x: element.x, y: element.y, radius}, element.inRange(vertex.x, vertex.y));
+              //expect(element.inRange(vertex.x, vertex.y)).withContext(`sides: ${sides}, rotation: ${rotation}, radius: ${radius}, borderWidth: ${borderWidth}, {x: ${vertex.x.toFixed(1)}, y: ${vertex.y.toFixed(1)}}`).toEqual(true);
             }
           }
         }
-      },
-    };
-
-    const polygonOpts = chartConfig.options.plugins.annotation.annotations.polygon;
-
-    it('should detect click event', function(done) {
-      const clickSpy = jasmine.createSpy('click');
-
-      polygonOpts.click = function(ctx) {
-        if (ctx.element.options.borderWidth) {
-          delete ctx.element.options.borderWidth;
-          ctx.chart.draw();
-        } else {
-          ctx.element.options.click = clickSpy;
-        }
-      };
-
-      const chart = window.acquireChart(chartConfig);
-      const xScale = chart.scales.x;
-      const yScale = chart.scales.y;
-      const eventPoint = {x: xScale.getPixelForValue(5), y: yScale.getPixelForValue(5)};
-
-      window.afterEvent(chart, 'click', function() {
-        expect(clickSpy.calls.count()).toBe(0);
-
-        window.afterEvent(chart, 'click', function() {
-          expect(clickSpy.calls.count()).toBe(1);
-          delete polygonOpts.click;
-          done();
-        });
-        window.triggerMouseEvent(chart, 'click', eventPoint);
       });
-      window.triggerMouseEvent(chart, 'click', eventPoint);
-    });
+/*
 
+
+      it(`should return false outside element '${element.options.id}'`, function() {
+        for (const borderWidth of [0, 10]) {
+          const halfBorder = borderWidth / 2;
+          element.options.borderWidth = borderWidth;
+          const radius = element.height / 2;
+          for (const angle of [0, 45, 90, 135, 180, 225, 270, 315]) {
+            const rad = angle * (Math.PI / 180);
+            const {x, y} = {
+              x: element.x + Math.cos(rad) * (radius + halfBorder + 1),
+              y: element.y + Math.sin(rad) * (radius + halfBorder + 1)
+            };
+            expect(element.inRange(x, y)).withContext(`angle: ${angle}, radius: ${radius}, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(false);
+          }
+        }
+      });
+*/
+    });
+/*
+
+    elemsNoRad.forEach(function(element) {
+      it(`should return false radius is 0 element '${element.options.id}'`, function() {
+        for (const borderWidth of [0, 10]) {
+          const halfBorder = borderWidth / 2;
+          element.options.borderWidth = borderWidth;
+          for (const x of [element.x - halfBorder, element.x + halfBorder]) {
+            expect(element.inRange(x, element.y)).toEqual(false);
+          }
+          for (const y of [element.y - halfBorder, element.y + halfBorder]) {
+            expect(element.inRange(element.x, y)).toEqual(false);
+          }
+        }
+      });
+    });
+*/    
   });
 });
