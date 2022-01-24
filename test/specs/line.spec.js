247a714cee8c30002ce67894b3c9e965bb146cc0
stockiNail <stocki.nail@gmail.com>
2022-01-24 15:56:10 +0100

    adds tests on line label with rotation

--- test/specs/line.spec.js
+++ test/specs/line.spec.js
@@ -24,24 +24,11 @@ describe('Line annotation', function() {
         position: 'start'
       }
     };
-    const annotation3 = {
-      type: 'line',
-      scaleID: 'y',
-      value: 2,
-      endValue: 8,
-      borderWidth: 10,
-      label: {
-        enabled: true,
-        content: 'Label of the element',
-        position: 'start'
-      }
-    };
 
-    const chart = window.scatter10x10({annotation1, annotation2, annotation3});
-    const elemsStraightLines = window.getAnnotationElements(chart).filter(el => el.options.value && !el.options.endValue);
-    const elemsLabels = window.getAnnotationElements(chart).filter(el => el.options.label && el.options.label.enabled);
+    const chart = window.scatter10x10({annotation1, annotation2});
+    const elems = window.getAnnotationElements(chart);
 
-    elemsStraightLines.forEach(function(element) {
+    elems.forEach(function(element) {
       const center = element.getCenterPoint();
       it('should return true inside element', function() {
         const halfBorder = element.options.borderWidth / 2;
@@ -61,30 +48,37 @@ describe('Line annotation', function() {
         }
       });
     });
+  });
 
-    elemsLabels.forEach(function(element) {
+  describe('inRange with rotation', function() {
+    const rotated = window.helpers.rotated;
+
+    for (const rotation of [0, 45, 90, 135, 180, 225, 270, 315]) {
+      const annotation = {
+        type: 'line',
+        scaleID: 'y',
+        value: 5,
+        label: {
+          enabled: true,
+          content: 'Label of the element',
+          rotation
+        }
+      };
+      const chart = window.scatter10x10({annotation});
+      const element = window.getAnnotationElements(chart)[0];
+
       it('should return true inside label of element', function() {
         for (const borderWidth of [0, 10]) {
           const halfBorder = borderWidth / 2;
           element.options.label.borderWidth = borderWidth;
-          for (const x of [element.labelX - element.labelWidth / 2 - halfBorder, element.labelX - element.labelWidth / 2 - halfBorder, element.labelX + element.labelWidth / 2 + halfBorder]) {
-            for (const y of [element.labelY - element.labelHeight / 2 - halfBorder, element.labelY - element.labelHeight / 2 - halfBorder, element.labelY + element.labelHeight / 2 + halfBorder]) {
-              expect(element.inRange(x, y)).withContext(`scaleID: ${element.options.scaleID}, value: ${element.options.value}, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(true);
-            }
-          }
+          const rad = rotation / 180 * Math.PI;
+          const {x, y} = rotated({
+            x: element.labelX + Math.cos(rad) * (element.labelWidth / 2 + halfBorder),
+            y: element.labelY + Math.sin(rad) * (element.labelHeight / 2 + halfBorder)
+          }, {x: element.labelX, y: element.labelY}, rad);
+          expect(element.inRange(x, y)).withContext(`rotation: ${rotation}, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(true);
         }
       });
-      it('should return false inside label of element', function() {
-        for (const borderWidth of [0, 10]) {
-          const halfBorder = borderWidth / 2;
-          element.options.label.borderWidth = borderWidth;
-          for (const x of [element.labelX - element.labelWidth / 2 - halfBorder - 1, element.labelX - element.labelWidth / 2 - halfBorder - 1, element.labelX + element.labelWidth / 2 + halfBorder - 1]) {
-            for (const y of [element.labelY - element.labelHeight / 2 - halfBorder - 1, element.labelY - element.labelHeight / 2 - halfBorder - 1, element.labelY + element.labelHeight / 2 + halfBorder + 1]) {
-              expect(element.inRange(x, y)).withContext(`scaleID: ${element.options.scaleID}, value: ${element.options.value}, borderWidth: ${borderWidth}, {x: ${x.toFixed(1)}, y: ${y.toFixed(1)}}`).toEqual(false);
-            }
-          }
-        }
-      });
-    });
+    }
   });
 });
