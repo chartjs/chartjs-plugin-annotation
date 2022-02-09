0f7fed0bca534b4e099490b268f6fbab5e4434f3
stockiNail <stocki.nail@gmail.com>
2022-02-09 17:36:55 +0100

    Add some missing element properties to annotation element type

--- types/element.d.ts
+++ types/element.d.ts
@@ -3,7 +3,15 @@ import { AnnotationOptions } from './options';
 export interface AnnotationElement {
   x: number,
   y: number,
-  x2: number,
-  y2: number,
+  x2?: number,
+  y2?: number,
+  width: number,
+  height: number,
+  pointX?: number,
+  pointY?: number,
+  labelX?: number,
+  labelY?: number,
+  labelWidth?: number,
+  labelHeight?: number,
   options: AnnotationOptions
 }
