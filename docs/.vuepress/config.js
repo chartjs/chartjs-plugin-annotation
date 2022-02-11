6aeb5c41af71a4cf5a58c26b253bf4929d0498fa
stockiNail <stocki.nail@gmail.com>
2022-02-11 12:18:01 +0100

    Add new samples to label in order to use canvas

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -122,6 +122,7 @@ module.exports = {
             'label/basic',
             'label/point',
             'label/callout',
+            'label/canvas',
             'label/lowerUpper',
           ]
         },
