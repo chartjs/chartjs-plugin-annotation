1f397d400893a9873a528d8519f33edcee7b0754
stockiNail <stocki.nail@gmail.com>
2022-05-24 13:43:32 +0200

    adds test cases

---
+++ test/fixtures/ellipse/labelCanvas.js
@@ -0,0 +1,101 @@
+module.exports = {
+  tolerance: 0.0060,
+  config: {
+    type: 'scatter',
+    options: {
+      scales: {
+        x: {
+          display: true,
+          min: -10,
+          max: 10
+        },
+        y: {
+          display: true,
+          min: -10,
+          max: 10
+        }
+      },
+      plugins: {
+        legend: false,
+        annotation: {
+          annotations: {
+            canvas1: {
+              type: 'ellipse',
+              xMin: -9,
+              xMax: -1,
+              yMin: 9,
+              yMax: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderWidth: 2,
+              label: {
+                display: true,
+                position: 'start',
+                content: window.createCanvas,
+                width: '25%',
+                height: '25%'
+              }
+            },
+            canvas2: {
+              type: 'ellipse',
+              xMin: 1,
+              xMax: 9,
+              yMin: 9,
+              yMax: 1,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderWidth: 2,
+              label: {
+                display: true,
+                position: 'end',
+                content: window.createCanvas,
+                width: '25%',
+                height: '25%'
+              }
+            },
+            canvas3: {
+              type: 'ellipse',
+              xMin: -9,
+              xMax: -1,
+              yMin: -1,
+              yMax: -9,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderWidth: 2,
+              label: {
+                display: true,
+                position: {
+                  x: 'start',
+                  y: 'center'
+                },
+                content: window.createCanvas,
+                width: '25%',
+                height: '25%'
+              }
+            },
+            canvas4: {
+              type: 'ellipse',
+              xMin: 1,
+              xMax: 9,
+              yMin: -1,
+              yMax: -9,
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderColor: 'rgba(255, 99, 132)',
+              borderWidth: 2,
+              label: {
+                display: true,
+                position: {
+                  x: 'center',
+                  y: 'end'
+                },
+                content: window.createCanvas,
+                width: '25%',
+                height: '25%'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
---
+++ test/fixtures/ellipse/labelDecoration.js
@@ -0,0 +1,84 @@
+module.exports = {
+  tolerance: 0.0320,
+  config: {
+    type: 'scatter',
+    options: {
+      scales: {
+        x: {
+          display: false,
+          min: 0,
+          max: 10
+        },
+        y: {
+          display: false,
+          min: 0,
+          max: 10
+        }
+      },
+      plugins: {
+        annotation: {
+          annotations: {
+            ellipse1: {
+              type: 'ellipse',
+              xMin: 0.5,
+              xMax: 9.5,
+              yMin: 6.5,
+              yMax: 9.5,
+              backgroundColor: '#f5f5f5',
+              borderColor: 'black',
+              borderWidth: 1,
+              label: {
+                display: true,
+                content: ['text stroke width 5', 'text stroke color red'],
+                textStrokeWidth: 5,
+                textStrokeColor: 'red',
+                font: {
+                  size: 40
+                }
+              }
+            },
+            ellipse2: {
+              type: 'ellipse',
+              xMin: 0.5,
+              xMax: 9.5,
+              yMin: 4,
+              yMax: 6,
+              backgroundColor: '#f5f5f5',
+              borderColor: 'black',
+              borderWidth: 1,
+              label: {
+                display: true,
+                textStrokeWidth: 3,
+                color: 'white',
+                content: 'text stroke width 3',
+                font: {
+                  size: 30
+                }
+              }
+            },
+            ellipse3: {
+              type: 'ellipse',
+              xMin: 0.5,
+              xMax: 9.5,
+              yMin: 0.5,
+              yMax: 3.5,
+              backgroundColor: '#f5f5f5',
+              borderColor: 'black',
+              borderWidth: 1,
+              label: {
+                display: true,
+                color: '#40E0D0',
+                content: ['text stroke width 10', 'text stroke color turq'],
+                textStrokeWidth: 10,
+                textStrokeColor: 'black',
+                font: {
+                  size: 40
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
