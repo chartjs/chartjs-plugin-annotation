6ca8aeccdb57e719153f4a9ccc553e95fdfa1113
stockiNail <stocki.nail@gmail.com>
2022-06-03 22:02:11 +0200

    fixes and improves test case on init animation

--- test/specs/animation.spec.js
+++ test/specs/animation.spec.js
@@ -1,27 +1,21 @@
-describe('Animation', function() {
+describe('Initial animation', function() {
 
-  for (const type of ['box', 'ellipse', 'label', 'line', 'point', 'polygon']) {
-    let count = 0;
+  const types = {
+    box: 'x',
+    ellipse: 'width',
+    label: 'x',
+    line: 'x',
+    point: 'radius',
+    polygon: 'y'
+  };
 
-    const plugin = {
-      id: 'initAnim',
-      afterInit() {
-        count = 0;
-      },
-      afterDraw(chart) {
-        const element = window.getAnnotationElements(chart)[0];
-        const {width} = element.getProps(['width'], true);
-        if (element.width === width) {
-          count++;
-        }
-        expect(count).toBeLessThanOrEqual(1);
-      }
-    };
-
+  for (const type of Object.keys(types)) {
     const chartConfig = {
       type: 'scatter',
-      plugins: [plugin],
       options: {
+        animation: {
+          duration: 500
+        },
         scales: {
           x: {
             display: false,
@@ -46,26 +40,124 @@ describe('Animation', function() {
 
     const options = {
       type,
-      xMin: 2,
+      xMin: 4,
       yMin: 4,
-      xMax: 8,
+      xMax: 6,
       yMax: 6,
+      radius: 40,
       borderWidth: 0
     };
 
-    const pluginOpts = chartConfig.options.plugins.annotation;
-    const commonOpts = pluginOpts.common;
+    it(`should reach the final position once in ${type} annotation`, function(done) {
 
-    it(`should reach the final position once in ${type} annotation`, function() {
+      const pluginOpts = chartConfig.options.plugins.annotation;
+      const commonOpts = pluginOpts.common;
+      const property = types[type];
+      let cycles = 0;
+
+      chartConfig.plugins = [{
+        id: 'initAnimEnabled',
+        afterInit(chart) {
+          chart.annotationCount = 0;
+        },
+        afterDraw(chart) {
+          let element = window.getAnnotationElements(chart)[0];
+          chart.initAnimation = element.options.initAnimation;
+          if (type === 'polygon') {
+            element = element.elements[0];
+          }
+          const opts = element.getProps([property], true);
+          const valueFinal = opts[property];
+          const value = element[property];
+          if (value === valueFinal) {
+            chart.annotationCount++;
+          }
+        },
+        afterRender(chart) {
+          cycles++;
+          if (cycles === 2) {
+            expect(chart.annotationCount <= 1 && chart.initAnimation).withContext(`with count ${chart.annotationCount}, initAnimation ${chart.initAnimation}`).toEqual(true);
+            done();
+          }
+        }
+      }];
 
       [commonOpts, options].forEach(function(targetOptions) {
+        delete commonOpts.initAnimation;
+        delete options.initAnimation;
         targetOptions.initAnimation = true;
         pluginOpts.annotations = [options];
         window.acquireChart(chartConfig);
-        delete targetOptions.initAnimation;
       });
 
     });
+
+    it(`should not update the position in ${type} annotation`, function(done) {
+
+      const pluginOpts = chartConfig.options.plugins.annotation;
+      const commonOpts = pluginOpts.common;
+      const property = types[type];
+      let cycles = 0;
+
+      chartConfig.plugins = [{
+        id: 'initAnimDisabled',
+        afterInit(chart) {
+          chart.annotationCount = 0;
+        },
+        afterDraw(chart) {
+          let element = window.getAnnotationElements(chart)[0];
+          chart.initAnimation = element.options.initAnimation;
+          if (type === 'polygon') {
+            element = element.elements[0];
+          }
+          const opts = element.getProps([property], true);
+          const valueFinal = opts[property];
+          const value = element[property];
+          if (value !== valueFinal) {
+            chart.annotationCount++;
+          }
+        },
+        afterRender(chart) {
+          expect(chart.annotationCount === 0 && !chart.initAnimation).withContext(`with count ${chart.annotationCount}, initAnimation ${chart.initAnimation}`).toEqual(true);
+          cycles++;
+          if (cycles === 2) {
+            done();
+          }
+        }
+      }];
+
+      [commonOpts, options].forEach(function(targetOptions) {
+        delete commonOpts.initAnimation;
+        delete options.initAnimation;
+        targetOptions.initAnimation = false;
+        pluginOpts.annotations = [options];
+        window.acquireChart(chartConfig);
+      });
+
+    });
+
+    it(` callback should not receive the element properties in ${type} annotation`, function(done) {
+
+      const pluginOpts = chartConfig.options.plugins.annotation;
+      const commonOpts = pluginOpts.common;
+      let cycles = 0;
+      
+      [commonOpts, options].forEach(function(targetOptions) {
+        delete commonOpts.initAnimation;
+        delete options.initAnimation;
+        targetOptions.initAnimation = function({chart, properties, options}) {
+          expect(typeof properties === 'object').toEqual(true);
+          cycles++;
+          if (cycles === 2) {
+            done();
+          }
+        };
+        pluginOpts.annotations = [options];
+        window.acquireChart(chartConfig);
+      });
+
+    });
+
   }
 
 });
